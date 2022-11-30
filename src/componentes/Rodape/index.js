3cea8ec2b2e734267c042340ac7c18eb119e9464
Vitor Lucas Fujita Felício <vlucasff@hotmail.com>
2022-11-30 14:10:43 -0300

    att

--- src/componentes/Rodape/index.js
+++ src/componentes/Rodape/index.js
@@ -1,11 +1,14 @@
 import styles from './Rodape.module.css';
-import { ReactComponent as MarcaRegistrada } from 'assets/marca_registrada.svg';
+import { ReactComponent as MarcaRegistrada } from 'assets/instagram.svg';
 
 export default function Rodape() {
   return (
     <footer className={styles.rodape} >
-        <MarcaRegistrada />
-        Desenvolvido com Alura
+        <a target="_blank" href='https://www.instagram.com/vichelly_/'> Instagram</a>
+        <div></div>
+        <div>/</div>
+        <div></div>
+        <a target="_blank" href="https://www.linkedin.com/in/vitor-lucas-fujita-fel%C3%ADcio-50a30622a/" >Linkedin</a>
     </footer>
   )
 }
--- src/paginas/SobreMim/index.js
+++ src/paginas/SobreMim/index.js
@@ -21,7 +21,19 @@ export default function SobreMim() {
                 className={styles.fotoSobreMim}
             />
         <p className={styles.paragrafo} >
-            Depois eu escrevo essa parada
+            Meu nome é Vitor Lucas Fujita Felício e tenho 19 anos. Meus hobbies são praticar esportes, jogar video game e sair com meus amigos e família. Moro em Santo André, estudo Ciência da Computação na FEI e atualmente estou estagiando na Agile Inc.  
+        </p>
+        <p className={styles.paragrafo} >
+            Comecei a programar em 2020 com cursos gratuitos na plataforma DIO, aprendi o básico de lógica de programação e tive meu primeiro contato com HTML e CSS.
+        </p>
+        <p className={styles.paragrafo} >
+            Em 2021, ano de pandemia, eu tive um contato mais intenso com a linguagem python e me apaixonei por programação, foi quando definitivamente decidi virar desenvolvedor e prestar vestibular para Ciência da Computação.
+        </p>
+        <p className={styles.paragrafo} >
+            Após uma dificil decisão optei por estudar na FEI, lá me aprofundei mais no desenvolvimento web e decidi que era essa área a que eu gostaria me aprimorar e ganhar maestria. Porém para me desenvolver mais percebi que seria necessário procurar um bom estágio na área
+        </p>
+        <p className={styles.paragrafo} >
+            Consegui a minha primeira entrevista na Agile Inc e consegui passar, agradeço aos meus pais, e aos empresa pela aposta que fizeram em mim. E pretendo evoluir muito mais profissionalmente, pessoalmente e espiritualmente daqui pra frente!
         </p>
 
     </PostModelo>
